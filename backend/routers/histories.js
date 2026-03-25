eab4f93b64226d99f138688a47e00350549cc41e
UlyssesTyson <ulyssestyson@yahoo.com>
2026-03-25 09:07:17 +0000

    deleting answer files

--- backend/routers/histories.js
+++ backend/routers/histories.js
@@ -2,12 +2,10 @@ const { Router } = require('express');
 
 // All the controllers required
 const questionController = require('../controllers/question')
-const answerController = require('../controllers/answer')
 const leaderboardController = require('../controllers/leaderboard')
 const accountController = require('../controllers/user');
 
 const questionRouter = Router();
-const answerRouter = Router();
 const leaderboardRouter = Router();
 const authRouter = Router();
 
@@ -18,10 +16,7 @@ leaderboardRouter.post("/home", leaderboardController.create) // this is to crea
 // questionRouter.get("/home", questionController.index); // get all questions
 questionRouter.get("/home/:category", questionController.show); // get questions with answers by category
 
-// answerRouter.get("/home/:QN", answerController.index) // placeholder
-// answerRouter.get("/home/:QN", answerController.show) // placeholder
-
 authRouter.post("/register", accountController.register); // Sign up route (create new account)
 authRouter.post("/login", accountController.login); // Login route (check username/password)
 
-module.exports = {questionRouter, leaderboardRouter, answerRouter, authRouter}
+module.exports = {questionRouter, leaderboardRouter, authRouter}
